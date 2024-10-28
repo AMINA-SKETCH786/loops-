22f2d216301b20e5256038ccbc63c8ebbddc1641
AMINA-SKETCH786 <aminna.readings@gmail.com>
2024-10-28 12:35:26 -0700

    Add files via upload

---
+++ loopi/index.js
@@ -0,0 +1,36 @@
+//for loop
+let cars = ["EOS", "TIGON", "PASSOT","TOURAN","JETTA"];
+
+prompt("please enter a car name ");
+console.log(cars);
+
+
+for(let i  = 0 ; i<cars.length; i++){
+  
+  console.log(`${cars}: ${cars}`);
+  
+
+document.getElementById("cars.length").innerHTML = cars;
+}
+
+//while loop
+
+//let cars = ["VW EOS", "VW TIGON", "VW PASSOT","VW TOURAN","VW JETTA"];
+//+prompt("enter your car number");
+//let i = 0;
+//while (i <=10) {
+    //console.log(` ${cars} / ${i} = ${cars*(i)}`)
+   // i++
+//}
+
+
+// do while loop
+//let number;
+//do{
+ // number = prompt("please enter a  car number between 0 and 700");
+//} while(!(number >= 0 && number < 700)){
+ // console.log("get your car!")
+//};
+
+
+
